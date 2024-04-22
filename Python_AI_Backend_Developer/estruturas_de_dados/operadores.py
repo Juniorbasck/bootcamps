b3b45fbb154114d24d61f6bf4e37d6b27c43b360
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-04-22 18:07:00 -0300

    atualizacoes

---
+++ Python_AI_Backend_Developer/estruturas_de_dados/operadores.py
@@ -0,0 +1,35 @@
+# operadores aritiméticos
+
+produto_1 = 10
+produto_2 =  20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+## operadores de comparação 
+
+saldo = 200
+saque = 200
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo <saque)
+
+## opereadores atribuição 
+
+valor = 500
+
+valor += 500
+valor -= 500
+valor /= 500
+valor //= 500
+valor *= 500
+
+print(valor)

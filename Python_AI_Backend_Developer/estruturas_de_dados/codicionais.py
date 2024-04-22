a86911769e3996c77add7132baa7ec0faaa2a497
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-04-22 19:45:36 -0300

    atualizacoes

---
+++ Python_AI_Backend_Developer/estruturas_de_dados/codicionais.py
@@ -0,0 +1,24 @@
+MAIOR_IDADE = 18 
+IDADE_ESPECIAL = 16
+
+idade = int(input("informa a idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("pode tirar a CNH")
+
+if idade < MAIOR_IDADE:
+    print("não pode tirar a CNH")
+
+
+if idade >= MAIOR_IDADE:
+    print("pode tirar a CNH")
+else:
+    print("não pode tirar a CNH")
+
+
+if idade >= MAIOR_IDADE:
+    print("pode tirar a CNH")
+elif idade == IDADE_ESPECIAL:
+    print("pode fazer aulas praticas")
+else :
+    print("não pode tirar a CNH")

8a29559121931b5716bef21c1a0411dae0afb292
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-04-23 22:40:08 -0300

    estruturas de reptição - python

---
+++ Python_AI_Backend_Developer/estruturas_de_dados/estruturas_rep.py
@@ -0,0 +1,29 @@
+texto = input("digite um palavra: ")
+VOGAIS = "AEIOU"
+
+
+for letra in texto: 
+    if letra.upper() in VOGAIS:
+        print(letra, end="")    
+else:
+    print()
+    print("executado")
+
+print()
+
+#range é uma sequencia de numero
+for numero in range(0,11):
+    print(numero, end="")
+
+#tabuada do 5
+for numero in range(0, 52, 5):
+    print(numero, end="")
+
+#estrutura while 
+while True:
+    numero = int(input("Informe um número: "))
+
+    if numero == 10:
+        break
+
+    print(numero);

82e1c0763dee51e7f4251a4c198134c2e043701e
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-04-24 21:30:40 -0300

    manipulate strings

--- Python_AI_Backend_Developer/estruturas_de_dados/manipulando_string.py
+++ Python_AI_Backend_Developer/estruturas_de_dados/manipulando_string.py
@@ -1,4 +1,6 @@
 nome = "teste"
+idade = 18
+profissãao = "engenheir"
 
 print(nome.upper())
 print(nome.lower())
@@ -10,8 +12,7 @@ print(nome.lstrip())
 print(nome.rstrip())
 
 
-
-
+print(f"meu nomes é {nome}, tenho {idade}, eu sou {profissãao}")
 
 
 

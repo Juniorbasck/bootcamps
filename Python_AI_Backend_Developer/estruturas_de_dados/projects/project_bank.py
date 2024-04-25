0d1760e684300cef9297e690152da211382bee37
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-04-25 15:07:36 -0300

    initializing project

---
+++ Python_AI_Backend_Developer/estruturas_de_dados/projects/project_bank.py
@@ -0,0 +1,29 @@
+
+mensagem = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+"""
+
+SAQUE_DIARIO = 0
+SALDO_CONTA = 0 
+
+
+
+while True: 
+
+    opcao = input(mensagem)
+    
+    if opcao == "d":
+        deposito = int(input("Digite o valor de deposito: "))
+        SALDO_CONTA = deposito + SALDO_CONTA
+    
+    if opcao == "s": 
+        if SAQUE_DIARIO <= 3 and SALDO_CONTA > 0:
+            saque = int(int(input("Dite o valor a ser sacado: ")))
+            
+            
+        

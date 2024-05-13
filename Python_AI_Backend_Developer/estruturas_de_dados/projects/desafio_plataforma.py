33428b966ce1a1e6f015a0d07f7cbc7c56c078c8
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-05-13 18:06:58 -0300

    OPP python

---
+++ Python_AI_Backend_Developer/estruturas_de_dados/projects/desafio_plataforma.py
@@ -0,0 +1,11 @@
+itens = []
+
+for i in range(4):
+  item = input()
+  itens.append(item)
+  
+# Exibe a lista de itens
+print("Lista de Equipamentos:")  
+for item in itens:
+    # Loop que percorre cada item na lista "itens"
+    print(f"- {item}")
